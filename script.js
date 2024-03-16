b87c43fcaed459e283b4c34bc7663899d2f68006
AryCh81 <163639027+AryCh81@users.noreply.github.com>
2024-03-16 17:45:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// let mf = document.getElementsByClassName('mf1');
+// for(let i=0;i<5;i++){
+//     mf.style.position = 'static';
+// }
+// let i=0;
+// document.body.addEventListener('click',()=>{
+//     mf[i++].style.display = 'block';
+// })
